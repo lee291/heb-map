a32d291293eebc60ce4e2a517e9c260c40ff6aa4
ylee-projects <141315831+ylee-projects@users.noreply.github.com>
2023-08-07 19:54:33 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,247 @@
+var data = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.71173143863004, 30.260243432001733]
+      },
+      "properties": {
+        "name": "7th Street H-E-B",
+        "address": "2701 EAST 7TH AUSTIN, TX 78702-3907"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.77586361534249, 30.284550607388063]
+      },
+      "properties": {
+        "name": "Lake Austin H-E-B",
+        "address": "2652 LAKE AUSTIN BLVD AUSTIN, TX 78703"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.72219291534248, 30.237381149228625]
+      },
+      "properties": {
+        "name": "Riverside H-E-B plus!",
+        "address": "2508 EAST RIVERSIDE DRIVE AUSTIN, TX 78741-3037"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.7199568, 30.30135972451489]
+      },
+      "properties": {
+        "name": "Hancock Center H-E-B",
+        "address": "1000 EAST 41 ST. AUSTIN, TX 78751-4810"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.6985554846575, 30.302006719746974]
+      },
+      "properties": {
+        "name": "Mueller H-E-B",
+        "address": "1801 E.51ST STREET AUSTIN, TX 78723-3014"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.74107208465749, 30.335181315043634]
+      },
+      "properties": {
+        "name": "Burnet Rd H-E-B",
+        "address": "5808 BURNET RD AUSTIN, TX 78756-1100"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.8249075846575, 30.29240871481224]
+      },
+      "properties": {
+        "name": "Westlake H-E-B",
+        "address": "701 CAPITAL OF TEXAS HWY-BLD C WEST LAKE HILLS, TX 78746-5256"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.76863216931501, 30.189447347847832]
+      },
+      "properties": {
+        "name": "I 35 and William Cannon H-E-B",
+        "address": "6607 SOUTH IH 35 AUSTIN, TX 78744-3410"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.7548765, 30.35255081367754]
+      },
+      "properties": {
+        "name": "Far West H-E-B",
+        "address": "7015 VILLAGE CTR DR. AUSTIN, TX 78731-3023"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.83087025397253, 30.21684680834418]
+      },
+      "properties": {
+        "name": "Brodie Lane H-E-B",
+        "address": "6900 BRODIE LANE AUSTIN, TX 78745-5008"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.69772842328754, 30.36530399589566]
+      },
+      "properties": {
+        "name": "Lamar and Rundberg H-E-B",
+        "address": "9414 N LAMAR BLVD AUSTIN, TX 78753-4106"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.78683870000002, 30.16993789788907]
+      },
+      "properties": {
+        "name": "Slaughter and South Congress H-E-B",
+        "address": "8801 SOUTH CONGRESS AVENUE AUSTIN, TX 78745"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.82494248465751, 30.176124145603854]
+      },
+      "properties": {
+        "name": "Slaughter and Manchaca H-E-B",
+        "address": "2110 WEST SLAUGHTER LN AUSTIN, TX 78748-5992"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.88777398465751, 30.22864917963926]
+      },
+      "properties": {
+        "name": "Oak Hill H-E-B",
+        "address": "7901 US-290 AUSTIN, TX 78736-8300"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.74843683068497, 30.399436878662254]
+      },
+      "properties": {
+        "name": "North Hills H-E-B",
+        "address": "10710 RESEARCH BLVD. STE 200 AUSTIN, TX 78759-5780"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.876631, 30.203229]
+      },
+      "properties": {
+        "name": "Slaughter and Escarpment H-E-B",
+        "address": "5800 W. SLAUGHTER LANE AUSTIN, TX 78749-6507"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.67167716931503, 30.40613076424893]
+      },
+      "properties": {
+        "name": "Tech Ridge H-E-B",
+        "address": "500 CANYON RIDGE DR. AUSTIN, TX 78753-1632"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.70281208465751, 30.418185662092416]
+      },
+      "properties": {
+        "name": "Parmer and Mopac H-E-B",
+        "address": "12407 N. MOPAC EXPWY AUSTIN, TX 78758-2475"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.85152816931502, 30.404891733429775]
+      },
+      "properties": {
+        "name": "Four Points H-E-B",
+        "address": "7301 N FM 620 AUSTIN, TX 78726-4539"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.772642, 30.434973537915774]
+      },
+      "properties": {
+        "name": "Spicewood Springs H-E-B",
+        "address": "12860 RESEARCH BLVD AUSTIN, TX 78750-3222"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.74240425397252, 30.442282482477328]
+      },
+      "properties": {
+        "name": "Parmer and McNeil H-E-B",
+        "address": "6001 WEST PARMER LANE AUSTIN, TX 78727-3901"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-97.82494250794505, 30.45431550003671]
+      },
+      "properties": {
+        "name": "Anderson Mill H-E-B plus!",
+        "address": "11521 NORTH FM620, BUILDING A AUSTIN, TX 78726-1168"
+      }
+    }
+  ]
+};
